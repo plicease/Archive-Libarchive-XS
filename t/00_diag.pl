04bde6ed8595bd733f27bf787be0cf47150a6526
Graham Ollis <perl@wdlabs.com>
2013-11-12 16:52:03 -0500

    don't fail diag if libarchive isn't there

--- t/00_diag.pl
+++ t/00_diag.pl
@@ -1,9 +1,14 @@
 use strict;
 use warnings;
 use Test::More;
-use Archive::Libarchive::XS qw( archive_version_string archive_version_number );
 
-diag "libarchive (str)  ", archive_version_string();
-diag "libarchive (int)  ", archive_version_number();
+diag "libarchive (str)  ", eval q{ 
+  use Archive::Libarchive::XS qw( archive_version_string );
+  archive_version_string();
+} || '-';
+diag "libarchive (int)  ", eval q{
+  use Archive::Libarchive::XS qw( archive_version_number );
+  archive_version_number();
+} || '-';
 
 1;
