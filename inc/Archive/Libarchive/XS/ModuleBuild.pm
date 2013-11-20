0134588fc506467643c618cc71093067ee099d7b
Graham Ollis <perl@wdlabs.com>
2013-11-20 11:32:32 -0500

    handle symbl detection when using system lib

--- inc/Archive/Libarchive/XS/ModuleBuild.pm
+++ inc/Archive/Libarchive/XS/ModuleBuild.pm
@@ -6,6 +6,8 @@ use base qw( Module::Build );
 use Alien::Libarchive;
 use File::Spec;
 use DynaLoader;
+use File::Temp qw( tempdir );
+use File::Spec;
 
 our $alien;
 
@@ -26,6 +28,8 @@ sub ACTION_build
 {
   my($self) = shift;
   
+  $alien ||= Alien::Libarchive->new;
+
   open(my $fh, '<', File::Spec->catfile('inc', 'symbols.txt'));
   my @symbols = <$fh>;
   close $fh;
@@ -34,17 +38,56 @@ sub ACTION_build
   open($fh, '>', File::Spec->catfile('xs', 'func.h'));
   print $fh "#ifndef FUNC_H\n";
   print $fh "#define FUNC_H\n\n";
-  
-  foreach my $symbol (@symbols)
+
+  if($alien->install_type('system'))
   {
-    print $fh "#define HAS_$symbol 1\n"
-      if DynaLoader::dl_find_symbol_anywhere($symbol);
+    foreach my $symbol (@symbols)
+    {
+      print $fh "#define HAS_$symbol 1\n"
+        if $self->_test_compile_symbol($symbol);
+    }
+  }
+  else
+  {
+    foreach my $symbol (@symbols)
+    {
+      print $fh "#define HAS_$symbol 1\n"
+        if DynaLoader::dl_find_symbol_anywhere($symbol);
+    }
   }
   
   print $fh "\n#endif\n";
   close $fh;
   
   $self->SUPER::ACTION_build(@_);
+}
+
+my $dir;
+my $count = 0;
+
+sub _test_compile_symbol
+{
+  my($self, $symbol) = @_;
+  
+  $dir = tempdir( CLEANUP => 1 ) unless $dir;
+  
+  my $fn = File::Spec->catfile($dir, "foo$count.c");
+  $count++;
+  open my $fh, '>', $fn;
+  print $fh "#include <archive.h>\n";
+  print $fh "#include <archive_entry.h>\n";
+  print $fh "int main(int argc, char *argv)\n";
+  print $fh "{ void *ptr = (void*)$symbol; }\n";
+  close $fh;
+  
+  my $cflags = $alien->cflags;
+  my $libs   = $alien->libs;
+  
+  #system "gcc $cflags $fn $libs";
+  #return ! $?;
+  
+  eval { $self->compile_c($fn) };
+  return $@ eq '';
 }
 
 1;
